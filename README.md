8a6881e3cf7df317900a82fef4b14530aa6d5b91
jesse keane <me@jessekeane.me>
2014-04-01 17:35:57 -0700

    updates

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-www-unpm
+www-&mu;npm
 ====
+
+a web frontend for &mu;npm
+
+## installation
+
+`npm install -g unpm-www`
+
+## usage
+
+`unpm-www`
+
+* `--port, -p <port>` Listen on `<port>`, defaults to `8999`
+* `--registry, -r <registry>` Use `<registry>` for package data
+* `--version, -v` Print version information
+* `--help, -h` Print help text
+
+## as a module
+
+## license
+
+MIT
--- bin/unpm-www.js
+++ bin/unpm-www.js
@@ -1,17 +1,37 @@
+var path = require('path')
+  , fs = require('fs')
+
 var nopt = require('nopt')
 
-var www = require('../')
+var package = require('../package.json')
+  , www = require('../')
 
 var noptions = {
     port: Number
   , registry: String
+  , help: Boolean
 }
 
 var shorts = {
     p: ['--port']
   , r: ['--registry']
+  , h: ['--help']
+  , v: ['--version']
 }
 
 var options = nopt(noptions, shorts)
 
+if(options.version) return version()
+if(options.help) return help()
+
 www(options.port, options.registry)
+
+function version() {
+  console.log(package.name + ' version ' + package.version)
+}
+
+function help() {
+  version()
+  fs.createReadStream(path.join(__dirname, '..', 'help.txt'))
+    .pipe(process.stderr)
+}
